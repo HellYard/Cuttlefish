226236bdb07a4a5cbb4146245dbc9a4809c60ac5
Daniel V <daniel.viddy@gmail.com>
2018-11-25 08:24:47 -0500

    Initial commit.

---
+++ src/com/hellyard/cuttlefish/api/Definition.java
@@ -0,0 +1,30 @@
+package com.hellyard.cuttlefish.api;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by creatorfromhell.
+ *
+ * Cuttlefish YAML Parser
+ *
+ * This work is licensed under the GNU Affero General Public License Version 3.
+ * To view a copy of this license, visit https://www.gnu.org/licenses/agpl-3.0.html.
+ */
+public class Definition {
+
+  private final String name;
+  private final Pattern regex;
+
+  public Definition(final String name, final Pattern regex) {
+    this.name = name;
+    this.regex = regex;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Pattern getRegex() {
+    return regex;
+  }
+}
---
+++ src/com/hellyard/cuttlefish/token/Token.java
@@ -0,0 +1,34 @@
+package com.hellyard.cuttlefish.token;
+
+/**
+ * Created by creatorfromhell.
+ *
+ * Cuttlefish YAML Parser
+ *
+ * This work is licensed under the GNU Affero General Public License Version 3.
+ * To view a copy of this license, visit https://www.gnu.org/licenses/agpl-3.0.html.
+ */
+public class Token {
+
+  private final int indentation;
+  private final String definition;
+  private final String value;
+
+  public Token(int indentation, String definition, String value) {
+    this.indentation = indentation;
+    this.definition = definition;
+    this.value = value;
+  }
+
+  public int getIndentation() {
+    return indentation;
+  }
+
+  public String getDefinition() {
+    return definition;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
