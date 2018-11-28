2fb4f14d57dc526234fe0d0d02f0680d8fd5e933
Articdive <articdive@gmail.com>
2018-11-28 16:38:19 +0100

    Added YamlNode & YamlGrammarizer

---
+++ src/com/hellyard/cuttlefish/definitions/yaml/ListEndDefinition.java
@@ -0,0 +1,19 @@
+package com.hellyard.cuttlefish.definitions.yaml;
+
+import com.hellyard.cuttlefish.api.definition.Definition;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by creatorfromhell.
+ *
+ * Cuttlefish YAML Parser
+ *
+ * This work is licensed under the GNU Affero General Public License Version 3. To view a copy of
+ * this license, visit https://www.gnu.org/licenses/agpl-3.0.html.
+ */
+public class ListEndDefinition extends Definition {
+  public ListEndDefinition() {
+    super("yaml_map", Pattern.compile(Pattern.quote("^\\s*?")));
+  }
+}
---
+++ src/com/hellyard/cuttlefish/definitions/yaml/ListSeparatorDefinition.java
@@ -0,0 +1,11 @@
+package com.hellyard.cuttlefish.definitions.yaml;
+
+import com.hellyard.cuttlefish.api.definition.Definition;
+
+import java.util.regex.Pattern;
+
+public class ListSeparatorDefinition extends Definition {
+    public ListSeparatorDefinition() {
+        super("yaml_list_separator", Pattern.compile(Pattern.quote(",")));
+    }
+}
